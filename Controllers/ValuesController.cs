4479f4b123f2394fa3d32a327a87e077e6f0863d
Edin Jašarević <edinjasar14@gmail.com>
2019-07-17 14:13:52 +0200

    production

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            throw new Exception("Test Exception");
             return new string[] { "value1", "value2" };
         }
 
