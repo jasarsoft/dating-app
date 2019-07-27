f7e427a8561f9626b0002e0ab6ccf9eeb47a5df3
Edin Jašarević <edinjasar14@gmail.com>
2019-07-27 15:10:56 +0200

    protect multiple rutes

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -6,9 +6,16 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-    { path: 'messages', component: MessagesComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' },    
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
