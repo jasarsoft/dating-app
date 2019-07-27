c31e2d4af345874ecad5035eddb4d4be8e643880
Edin Jašarević <edinjasar14@gmail.com>
2019-07-27 15:01:08 +0200

    protecting routes

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -38,7 +39,8 @@ import { appRoutes } from './routes';
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent },
+    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
     { path: 'messages', component: MessagesComponent },
     { path: 'lists', component: ListsComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' },    
