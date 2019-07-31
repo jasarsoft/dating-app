1ea2989a16013b74b11791a9a58379b991e90857
Edin Jašarević <edinjasar14@gmail.com>
2019-07-31 20:39:25 +0200

    ts interface

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,17 @@
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
+
