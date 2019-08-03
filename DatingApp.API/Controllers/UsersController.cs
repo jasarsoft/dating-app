b5c7c7e10b401688b8bf971749e480cdb9aae888
Edin Jašarević <edinjasar14@gmail.com>
2019-08-03 15:49:38 +0200

    persisting member

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -40,6 +42,22 @@ namespace DatingApp.API.Controllers
             var userToReturn = mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userForRepo = await repo.GetUser(id);
+
+            mapper.Map(userForUpdateDto, userForRepo);
+
+            if (await repo.SaveAll())
+                return NoContent();
+            
+            throw new Exception($"Updating user {id} falied on save");
         }
     }
 }
